cfc7f35691f1a96e67a5ad139c207d5e194ffa61
Crhisjairo <59382477+Crhisjairo@users.noreply.github.com>
2020-11-23 12:41:32 -0500

    Création du projet de base
    - Création du projet vide avec MainWindows.xaml et MainWindows.xaml.cs seulement

---
+++ Tp3/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Tp3
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
