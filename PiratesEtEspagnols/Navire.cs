dac5efcf73144efa33ba4802d1a831a3120c3ed0
Crhisjairo <59382477+Crhisjairo@users.noreply.github.com>
2020-11-23 16:32:00 -0500

    Ajout du projet PiratesEtEspagnol
    - Ajout du projet PiratesEtEspagnol  (nom temporaire)

---
+++ PiratesEtEspagnols/Navire.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PiratesEtEspagnols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
